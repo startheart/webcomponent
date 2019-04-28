5f7373c029c1e5d4e13322e4d4f17b620204353c
startheart <704469176@qq.com>
2019-04-28 18:51:28 +0800

    update

---
+++ wx/fancy-tabs.js
@@ -0,0 +1,27 @@
+// fancy-tabs.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    click(e) {
+      this.triggerEvent('myevent', {
+        content: e
+      })
+    }
+  }
+})
